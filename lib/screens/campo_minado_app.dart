8512dc2f5a02c07a4eed25c06d56202bbd12b9a1
Alan Perdomo <alanperdomo@hotmail.com>
2024-08-13 14:18:56 -0300

    #442

---
+++ lib/screens/campo_minado_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../components/resultado_widget.dart';
+
+class CampoMinadoApp extends StatelessWidget {
+  const CampoMinadoApp({super.key});
+
+  _reiniciar() {
+    print('reiniciar...');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: ResultadoWidget(
+          venceu: false,
+          onReiniciar: _reiniciar,
+        ),
+        body: const SizedBox(
+          child: Text('Tabuleiro'),
+        ),
+      ),
+    );
+  }
+}
